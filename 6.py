1dda785b267b88b2f54db42925ef995564c3a3e0
zevs5 <zevs57@gmail.com>
2021-09-01 14:41:57 +0300

    Сделать шарики двигающимися со случайным отражением от стен.

--- 6.py
+++ 6.py
@@ -19,19 +19,18 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 def new_ball():
     '''рисует новый шарик '''
-    global x, y, r
-    x = randint(100, 700)
-    y = randint(100, 500)
+    global x, y, r, color
     r = randint(30, 50)
+    x = randint(0 + r, 700)
+    y = randint(0 + r, 500)
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
 
 
 def click(event):
     """
     По клику мыши вычисляется (по теореме Пифагора) попали ли мы в шарик
     :param event:
-    :return:
+    :return: Возвращает True, если было попадание в шарик
     """
     if (event.pos[1] - y) ** 2 + (event.pos[0] - x) ** 2 <= r ** 2:
         return True
@@ -41,10 +40,22 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+new_ball()
 points = 0
 
+rect_change_x = randint(10, 50)  # Вектор скорости и направления
+rect_change_y = randint(10, 50)
 while not finished:
     clock.tick(FPS)
+    x += rect_change_x
+    y += rect_change_y
+    circle(screen, color, (x, y), r)
+    if y > 900 - r or y < 0 + r:  # Смена направления после удара о границу
+        rect_change_y += randint(10, 50)
+        rect_change_y = rect_change_y * -1
+    if x > 1200 - r or x < 0 + r:  # Смена направления после удара о границу
+        rect_change_x += randint(10, 50)
+        rect_change_x = rect_change_x * -1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -52,7 +63,6 @@ while not finished:
             if click(event):  # Посчет очков
                 points = points + 1
 
-    new_ball()
     pygame.display.update()
     screen.fill(BLACK)
 
